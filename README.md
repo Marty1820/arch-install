fb7f297928a7194d886792beb43729c8834e1528
Marty1820 <nickm1820@gmail.com>
2022-12-26 20:47:33 -0600

    fixed spacing

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # Marty's Computer Setup [Arch]
 
 # USE AT YOUR OWN RISK!
+
 An arch linux installation script for my personal use. Works on my Frame.Work Laptop!
 
 ## What this sets up
-+ UEFI - BIOS maybe done in the future...depending on how much work I want to do
-+ BTRFS - subvolumes for `/home` `/srv` `/var/log` `/var/cache` `/tmp` `/.snapshots` `/swap`
-+ SWAPFile - Mounts at `/swap/swafile`
+
+- UEFI - BIOS maybe done in the future...depending on how much work I want to do
+- BTRFS - subvolumes for `/home` `/srv` `/var/log` `/var/cache` `/tmp` `/.snapshots` `/swap`
+- SWAPFile - Mounts at `/swap/swafile`
 
 **`post_chroot` is not complete**
 
 ## Installation instructions after booting arch iso and connecting to network
+
 1. `pacman -Sy git`
 1. `git clone https://github.com/Marty1820/arch-install.git`
 1. `cd arch-install`
--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ echo -ne "
   after formating your disk there is no way to get data back
 ----------------------------------------------------------------
 "
-read -rp "Enter to continue <ctrl + c> to cancel"</dev/tty
+read -rp "Enter to continue <ctrl + c> to cancel" </dev/tty
 
 # Update the system clock
 timedatectl set-ntp true
 timedatectl status
 echo "Is the 'NTP service: active'?"
-read -rp "Enter to continue <ctrl + c> to cancel"</dev/tty
+read -rp "Enter to continue <ctrl + c> to cancel" </dev/tty
 
 # Partition the disks
 echo "Disk & Swap setup"
@@ -30,18 +30,18 @@ if [[ -d /sys/firmware/efi/efivars ]]; then
   echo "Boot mode UEFI"
   BOOT=uefi
   (
-  echo g # Create a new empty GPT partition table
-  echo n # Add a new partition
-  echo 1 # Partition number
-  echo   # First sector (Accept default: 1)
-  echo +300M # Last sector (adds 512M space for EFI)
-  echo t # Changing partition type
-  echo 1 # Set type to EFI
-  echo n # Add a new partition
-  echo 2 # Partition number
-  echo   # First sector (Accept default: varies)
-  echo   # Last sector (Accept default: varies)
-  echo w # Write changes
+    echo g     # Create a new empty GPT partition table
+    echo n     # Add a new partition
+    echo 1     # Partition number
+    echo       # First sector (Accept default: 1)
+    echo +300M # Last sector (adds 512M space for EFI)
+    echo t     # Changing partition type
+    echo 1     # Set type to EFI
+    echo n     # Add a new partition
+    echo 2     # Partition number
+    echo       # First sector (Accept default: varies)
+    echo       # Last sector (Accept default: varies)
+    echo w     # Write changes
   ) | fdisk /dev/"$DISK"
 
   # NVME vs SSD/HDD
@@ -63,7 +63,7 @@ if [[ -d /sys/firmware/efi/efivars ]]; then
 
   fdisk -l
   echo "Do the partitions look ok?"
-  read -rp "Enter to continue <ctrl + c> to cancel"</dev/tty
+  read -rp "Enter to continue <ctrl + c> to cancel" </dev/tty
 
   # Create btrfs volumes
   echo "Creating btrfs subvolumes."
@@ -81,7 +81,7 @@ if [[ -d /sys/firmware/efi/efivars ]]; then
   ls /mnt
   echo ""
   echo "Are all subvolumes shown?"
-  read -rp "Enter to continue <ctrl + c> to cancel"</dev/tty
+  read -rp "Enter to continue <ctrl + c> to cancel" </dev/tty
   umount /mnt
 
   # Mount / subvolume
@@ -101,10 +101,10 @@ if [[ -d /sys/firmware/efi/efivars ]]; then
   mount /dev/"${partition}"1 /mnt/boot
   lsblk /dev/"${DISK}"
   echo "Are partitions/subvolumes mounted?"
-  read -rp "Enter to continue <ctrl + c> to cancel"</dev/tty
+  read -rp "Enter to continue <ctrl + c> to cancel" </dev/tty
 
   #Setting up SWAP
-  ((SWAP=$SWAP_SIZE*1024))
+  ((SWAP = $SWAP_SIZE * 1024))
   truncate -s 0 /mnt/swap/swapfile
   chattr +C /mnt/swap/swapfile
   btrfs property set /mnt/swap/swapfile compression none
@@ -121,7 +121,7 @@ else
 fi
 
 # CPU information
-proc=$(grep vendor_id < /proc/cpuinfo | awk 'NR==1 {print $3}')
+proc=$(grep vendor_id </proc/cpuinfo | awk 'NR==1 {print $3}')
 if [[ $proc == GenuineIntel ]]; then
   ucode=intel-ucode
 elif [[ $proc == AuthenticAMD ]]; then
@@ -135,14 +135,13 @@ printf "linux\nlinux-hardened\nlinux-lts\nlinux-zen\n"
 read -rp "Please type in your kernel: " kern
 read -rp "Do you want headers installed? [y|N] " header
 case ${header:0:1} in
-  Y|y|yes|Yes )
-    kern="$kern ${kern}-headers"
-    ;;
-  * )
-    echo "No headers will be installed"
-    ;;
+Y | y | yes | Yes)
+  kern="$kern ${kern}-headers"
+  ;;
+*)
+  echo "No headers will be installed"
+  ;;
 esac
-
 
 # Install essential packages
 echo "Installing essential packages."
@@ -151,7 +150,7 @@ pacstrap /mnt base base-devel "$ucode" "$kern" linux-firmware \
 
 # Generate an fstab file
 echo "Generating fstab file."
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >>/mnt/etc/fstab
 cat /mnt/etc/fstab
 sleep 2
 
@@ -160,8 +159,8 @@ echo "Changing root into the new system."
 {
   echo -e "#!/bin/bash"
   "DISK=$DISK BOOT=$BOOT"
-} >> install.sh
-cat post_chroot >> install2.sh
+} >>install.sh
+cat post_chroot >>install2.sh
 cp install2.sh /mnt/
 chmod +x /mnt/install2.sh
 arch-chroot /mnt ./install2.sh
